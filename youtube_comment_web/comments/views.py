381da69a4d63e93dd025d578e99932ea994b8c2e
airpark69 <50907018+airpark69@users.noreply.github.com>
2023-05-03 20:47:50 +0900

    mod_comments_index_and_views

--- youtube_comment_web/comments/views.py
+++ youtube_comment_web/comments/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
-
+from .models import *
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    all_video = Video.objects.all()
+    context = {'first_video': all_video[0].data}
+    return render(request, 'polls/index.html', context)
