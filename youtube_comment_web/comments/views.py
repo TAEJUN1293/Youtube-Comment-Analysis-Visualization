88784eb7b72fafb8e1cb4265bc138a39cfc428c9
airpark69 <50907018+airpark69@users.noreply.github.com>
2023-05-03 20:49:02 +0900

    fix_views_error

--- youtube_comment_web/comments/views.py
+++ youtube_comment_web/comments/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def index(request):
     all_video = Video.objects.all()
     context = {'first_video': all_video[0].data}
-    return render(request, 'polls/index.html', context)
+    return render(request, 'comments/index.html', context)
