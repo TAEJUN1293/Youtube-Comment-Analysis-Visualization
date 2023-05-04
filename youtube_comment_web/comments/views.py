8f0eb76c6e591e25101391b269c0f80256297c9f
ha6oon <hayun-lee@naver.com>
2023-05-04 16:21:51 +0900

    templetes 추가

--- youtube_comment_web/comments/views.py
+++ youtube_comment_web/comments/views.py
@@ -6,3 +6,8 @@ def index(request):
     all_video = Video.objects.all()
     context = {'first_video': all_video[0].data}
     return render(request, 'comments/index.html', context)
+
+def detail(request):
+    all_video = Video.objects.all()
+    context = {'first_video': all_video[0].data}
+    return render(request, 'comments/detail.html', context)
